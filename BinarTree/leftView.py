07f5df346e0aae8addcf66e5d8a42d57192a2cb4
ekmahama <mahamaek@gmail.com>
2020-11-17 14:17:46 -0500

    recur added

--- BinarTree/leftView.py
+++ BinarTree/leftView.py
@@ -4,7 +4,7 @@ class Node:
         self.left = left
         self.right = right
 
-from collections import deque
+from collections import deque, defaultdict
 
 def leftView1(root):
     res = []
@@ -30,6 +30,21 @@ def leftView1(root):
                 q.append(cur.right)
     return res
 
+def leftView_2(root):
+    res = defaultdict(list)
+    if not root:
+        return res
+    def helper(start, level=0):
+        if start:
+            if level not in res: # if level is visited for the first time insert the current node
+                res[level].append(start.val)
+
+            helper(start.left, level +1)
+            helper(start.right, level + 1)
+
+    helper(root)
+    return res
+    
 if __name__ == "__main__":
     root = Node(1)
     root.left = Node(2)
@@ -40,4 +55,4 @@ if __name__ == "__main__":
     root.right.left.left = Node(7)
     root.right.left.right = Node(8)
 
-    res = leftView1(root)
+    res = leftView_2(root)
